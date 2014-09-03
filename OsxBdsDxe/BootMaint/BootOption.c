Furtif/Clover

commit b9e77137a410b0e67ddfff8a8bf614a7d0d146e2

Updated EFI filesystem drivers by AnV
git-svn-id: https://svn.code.sf.net/p/cloverefiboot/code@2819 52d7a1c0-eba5-4e63-a9d7-e3186cd7c462

OsxBdsDxe/BootMaint/BootOption.c
@@ -229,13 +229,17 @@ BOpt_FindFileSystem (
   )
 {
   UINTN                     NoBlkIoHandles;
+  UINTN                     NoBlkIoHandles2;
   UINTN                     NoSimpleFsHandles;
   UINTN                     NoLoadFileHandles;
   EFI_HANDLE                *BlkIoHandle;
+  EFI_HANDLE                *BlkIoHandle2;
   EFI_HANDLE                *SimpleFsHandle;
   EFI_HANDLE                *LoadFileHandle;
   UINT16                    *VolumeLabel;
   EFI_BLOCK_IO_PROTOCOL     *BlkIo;
+  EFI_BLOCK_IO2_PROTOCOL    *BlkIo2;
+  EFI_BLOCK_IO2_TOKEN       BlkIo2Token;
   UINTN                     Index;
   EFI_STATUS                Status;
   BM_MENU_ENTRY             *MenuEntry;
@@ -258,6 +262,54 @@ BOpt_FindFileSystem (
   InitializeListHead (&FsOptionMenu.Head);
 
   //
+  // Locate Handles that support BlockIo 2 protocol
+  //
+  Status = gBS->LocateHandleBuffer (
+                                    ByProtocol,
+                                    &gEfiBlockIo2ProtocolGuid,
+                                    NULL,
+                                    &NoBlkIoHandles2,
+                                    &BlkIoHandle2
+                                    );
+  if (!EFI_ERROR(Status))
+  {
+  for (Index = 0; Index < NoBlkIoHandles2; Index++) {
+        Status = gBS->HandleProtocol (
+                                      BlkIoHandle2[Index],
+                                      &gEfiBlockIo2ProtocolGuid,
+                                      (VOID **) &BlkIo2
+                                      );
+        if (EFI_ERROR(Status))
+        {
+            continue;
+        }
+        
+        //
+        // Issue a dummy read to trigger reinstall of BlockIo protocol for removable media0
+        //
+        if (BlkIo2->Media->RemovableMedia) {
+            Buffer = AllocateZeroPool (BlkIo2->Media->BlockSize);
+            if (NULL == Buffer) {
+                FreePool (BlkIoHandle2);
+                return EFI_OUT_OF_RESOURCES;
+            }
+
+             BlkIo2->ReadBlocksEx (
+                                      BlkIo2,
+                                      BlkIo2->Media->MediaId,
+                                      0,
+                                      &BlkIo2Token,
+                                      BlkIo2->Media->BlockSize,
+                                      Buffer
+                                      );
+            FreePool (Buffer);
+        }
+
+      FreePool (BlkIoHandle2);
+  }
+  }
+
+  //
   // Locate Handles that support BlockIo protocol
   //
   Status = gBS->LocateHandleBuffer (
@@ -268,7 +320,6 @@ BOpt_FindFileSystem (
                   &BlkIoHandle
                   );
   if (!EFI_ERROR (Status)) {
-
     for (Index = 0; Index < NoBlkIoHandles; Index++) {
       Status = gBS->HandleProtocol (
                       BlkIoHandle[Index],
@@ -281,7 +332,7 @@ BOpt_FindFileSystem (
       }
 
       //
-      // Issue a dummy read to trigger reinstall of BlockIo protocol for removable media
+      // Issue a dummy read to trigger reinstall of BlockIo protocol for removable media0
       //
       if (BlkIo->Media->RemovableMedia) {
         Buffer = AllocateZeroPool (BlkIo->Media->BlockSize);
@@ -297,10 +348,10 @@ BOpt_FindFileSystem (
                 BlkIo->Media->BlockSize,
                 Buffer
                 );
-        FreePool (Buffer);
-      }
+      FreePool (Buffer);
     }
     FreePool (BlkIoHandle);
+  }
   }
 
   //
@@ -320,6 +371,16 @@ BOpt_FindFileSystem (
     for (Index = 0; Index < NoSimpleFsHandles; Index++) {
       Status = gBS->HandleProtocol (
                       SimpleFsHandle[Index],
+                      &gEfiBlockIo2ProtocolGuid,
+                      (VOID **) &BlkIo2
+                      );
+      if (EFI_ERROR(Status))
+      {
+        BlkIo2 = NULL;
+      }
+
+      Status = gBS->HandleProtocol (
+                      SimpleFsHandle[Index],
                       &gEfiBlockIoProtocolGuid,
                       (VOID **) &BlkIo
                       );
@@ -332,7 +393,12 @@ BOpt_FindFileSystem (
         //
         // If block IO exists check to see if it's remobable media
         //
-        RemovableMedia = BlkIo->Media->RemovableMedia;
+        if (BlkIo2 != NULL)
+        {
+          RemovableMedia = BlkIo2->Media->RemovableMedia;
+        } else {
+          RemovableMedia = BlkIo->Media->RemovableMedia;
+        }
       }
 
       //

commit 0cf9141b39d68c6ce865007947af6e03ac721d5a

Correct mistake introduced in r2819: FreePool at wrong place
git-svn-id: https://svn.code.sf.net/p/cloverefiboot/code@2867 52d7a1c0-eba5-4e63-a9d7-e3186cd7c462

OsxBdsDxe/BootMaint/BootOption.c
@@ -271,42 +271,39 @@ BOpt_FindFileSystem (
                                     &NoBlkIoHandles2,
                                     &BlkIoHandle2
                                     );
-  if (!EFI_ERROR(Status))
-  {
-  for (Index = 0; Index < NoBlkIoHandles2; Index++) {
-        Status = gBS->HandleProtocol (
-                                      BlkIoHandle2[Index],
-                                      &gEfiBlockIo2ProtocolGuid,
-                                      (VOID **) &BlkIo2
-                                      );
-        if (EFI_ERROR(Status))
-        {
-            continue;
-        }
-        
-        //
-        // Issue a dummy read to trigger reinstall of BlockIo protocol for removable media0
-        //
-        if (BlkIo2->Media->RemovableMedia) {
-            Buffer = AllocateZeroPool (BlkIo2->Media->BlockSize);
-            if (NULL == Buffer) {
-                FreePool (BlkIoHandle2);
-                return EFI_OUT_OF_RESOURCES;
-            }
+  if (!EFI_ERROR(Status)) {
+    for (Index = 0; Index < NoBlkIoHandles2; Index++) {
+      Status = gBS->HandleProtocol (
+                                    BlkIoHandle2[Index],
+                                    &gEfiBlockIo2ProtocolGuid,
+                                    (VOID **) &BlkIo2
+                                    );
+      if (EFI_ERROR(Status)) {
+        continue;
+      }
 
-             BlkIo2->ReadBlocksEx (
-                                      BlkIo2,
-                                      BlkIo2->Media->MediaId,
-                                      0,
-                                      &BlkIo2Token,
-                                      BlkIo2->Media->BlockSize,
-                                      Buffer
-                                      );
-            FreePool (Buffer);
+      //
+      // Issue a dummy read to trigger reinstall of BlockIo protocol for removable media
+      //
+      if (BlkIo2->Media->RemovableMedia) {
+        Buffer = AllocateZeroPool (BlkIo2->Media->BlockSize);
+        if (NULL == Buffer) {
+          FreePool (BlkIoHandle2);
+          return EFI_OUT_OF_RESOURCES;
         }
 
-      FreePool (BlkIoHandle2);
-  }
+        BlkIo2->ReadBlocksEx (
+                              BlkIo2,
+                              BlkIo2->Media->MediaId,
+                              0,
+                              &BlkIo2Token,
+                              BlkIo2->Media->BlockSize,
+                              Buffer
+                              );
+        FreePool (Buffer);
+      }
+    }
+    FreePool (BlkIoHandle2);
   }
 
   //
@@ -332,7 +329,7 @@ BOpt_FindFileSystem (
       }
 
       //
-      // Issue a dummy read to trigger reinstall of BlockIo protocol for removable media0
+      // Issue a dummy read to trigger reinstall of BlockIo protocol for removable media
       //
       if (BlkIo->Media->RemovableMedia) {
         Buffer = AllocateZeroPool (BlkIo->Media->BlockSize);
@@ -348,10 +345,10 @@ BOpt_FindFileSystem (
                 BlkIo->Media->BlockSize,
                 Buffer
                 );
-      FreePool (Buffer);
+        FreePool (Buffer);
+      }
     }
     FreePool (BlkIoHandle);
-  }
   }
 
   //
@@ -374,8 +371,7 @@ BOpt_FindFileSystem (
                       &gEfiBlockIo2ProtocolGuid,
                       (VOID **) &BlkIo2
                       );
-      if (EFI_ERROR(Status))
-      {
+      if (EFI_ERROR(Status)) {
         BlkIo2 = NULL;
       }
 
@@ -393,8 +389,7 @@ BOpt_FindFileSystem (
         //
         // If block IO exists check to see if it's remobable media
         //
-        if (BlkIo2 != NULL)
-        {
+        if (BlkIo2 != NULL) {
           RemovableMedia = BlkIo2->Media->RemovableMedia;
         } else {
           RemovableMedia = BlkIo->Media->RemovableMedia;

SataControllerDxe/SataController.c
@@ -476,16 +476,17 @@ SataControllerStart (
     goto Done;
   }
 
-  // Some controllers (ex. Qemu Ahci) don't have enumeration data ready yet at this stage
-  // To solve this, the following code was moved to first NotifyPhase (EfiIdeBeforeChannelEnumeration)
-
   if (IS_PCI_IDE (&PciData)) {
     SataPrivateData->IdeInit.ChannelCount = IDE_MAX_CHANNEL;
     SataPrivateData->DeviceCount = IDE_MAX_DEVICES;
     SataPrivateData->IPorts = (1 << IDE_MAX_CHANNEL) - 1; //mask for N channels
     DBG(L"IDE controller found\n");
   } 
-/*  else if (IS_PCI_SATADPA (&PciData) || IS_PCI_RAID(&PciData)) {
+
+  // Some Sata controllers (ex. Qemu Ahci) don't have enumeration data ready yet at this stage
+  // To solve this, the following code was moved to first NotifyPhase (EfiIdeBeforeChannelEnumeration)
+/*  
+    else if (IS_PCI_SATADPA (&PciData) || IS_PCI_RAID(&PciData)) {
     //
     // Read Host Capability Register(CAP) to get Number of Ports(NPS) and Supports Port Multiplier(SPM)
     //   NPS is 0's based value indicating the maximum number of ports supported by the HBA silicon.
@@ -773,7 +774,7 @@ IdeInitNotifyPhase (
 
   if (Phase == EfiIdeBeforeChannelEnumeration) {
     // Initalize SATA mode Channels/Devices count and relevant structures before enumeration starts
-    // Notification comes from AtaAtapi by both AhciModeInitialization()
+    // Notification comes from AtaAtapi by AhciModeInitialization()
 
     SataPrivateData = SATA_CONTROLLER_PRIVATE_DATA_FROM_THIS (This);
     if (!SataPrivateData) {